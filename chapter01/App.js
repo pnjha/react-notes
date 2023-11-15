8989ce13926d22ca16ca99543880b70bdd81c91e
Prakash Nath Jha <prakashecdrid@gmail.com>
2023-11-15 07:15:53 +0530

    feat: chapter 01 part 3

---
+++ chapter01/App.js
@@ -0,0 +1,25 @@
+const heading = React.createElement("h1", { id: "heading" }, "hello world from react"); // {} adds attritbutes to a element
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// console.log(root);
+
+// root.render(heading);
+
+/**
+ *
+ * <div id="level0">
+ *  <div id="level1">
+ *    <h1>level2</h1>
+ *  </div>
+ * </div>
+ */
+
+const level2 = React.createElement("h1", {}, "level2");
+const level3 = React.createElement("h1", {}, "level3");
+const level1 = React.createElement("div", { id: "level1" }, [level2, level3]);
+const level0 = React.createElement("div", { id: "level0" }, level1);
+
+root.render(level0);
