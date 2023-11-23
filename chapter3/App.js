f4a7aa011e6189d387715c0c307c18b030e2353e
Prakash Nath Jha <prakashecdrid@gmail.com>
2023-11-23 09:52:46 +0530

    feat: end of episode 3

--- chapter3/App.js
+++ chapter3/App.js
@@ -1,18 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "hello world from react");
+const heading = React.createElement("h1", { id: "headingreact" }, "hello world from react");
 
-const headingJsx = (
-  <h1 className="heading" tabIndex="10">
-    {" "}
-    hello from jsx
+const testHeadingElement = (
+  <h1 className="headingjsx" tabIndex="10">
+    hello from test jsx element
   </h1>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// this is react element
+const HeadingElement = (
+  <div>
+    <h1 className="headingjsx" tabIndex="10">
+      hello from jsx element
+    </h1>
+    {testHeadingElement}
+  </div>
+);
 
-root.render(headingJsx);
+// this is react  component
+const HeadingJsx = () => (
+  <h1 className="headingjsx" tabIndex="10">
+    hello from jsx component
+  </h1>
+);
+
+const number = 1000000;
+
+// react component
+// two types of components: class based(old method) and functional
+// component composition
+const HeadingComponent = () => (
+  <div id="parent">
+    <h1 className="funcHeading" tabIndex="9">
+      hello react jsx functional composed component
+    </h1>
+    <HeadingJsx />
+    {HeadingJsx()}
+    {HeadingElement}
+    {heading}
+    <h2>{number}</h2>
+    <h2>{"{ a: 1 }"}</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+// root.render(HeadingComponent());
 
 // both heading and headingJsx objects are same wrt the browser
 // parcel via babel is converting the jsx object to proper js object aka transpiling
